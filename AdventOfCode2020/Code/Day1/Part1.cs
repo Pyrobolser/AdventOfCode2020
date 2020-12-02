498db293d0bf5d1e8019def90bc8249ca8ebf04b
Francois Blondel <fblondel@vivaldicap.com>
2020-12-01 18:15:52 -0600

    Add project files.

---
+++ AdventOfCode2020/Code/Day1/Part1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020.Code.Day1
+{
+    public static class Part1
+    {
+        private static int[] _expenses;
+        private const int TOTAL = 2020;
+
+        public static int Solve()
+        {
+            _expenses = Array.ConvertAll(File.ReadAllLines(@"Input\Day1.txt"), int.Parse);
+            var map = new HashSet<int>();
+            int left = 0, right = 0;
+
+            for(int i = 0; i < _expenses.Length; i++)
+            {
+                var temp = TOTAL - _expenses[i];
+
+                if(map.Contains(temp))
+                {
+                    left = _expenses[i];
+                    right = temp;
+                }
+                else
+                {
+                    map.Add(_expenses[i]);
+                }
+            }
+
+            return left * right;
+        }
+    }
+
+    public static class Part2
+    {
+        private static int[] _expenses;
+        private const int TOTAL = 2020;
+
+        public static int Solve()
+        {
+            _expenses = Array.ConvertAll(File.ReadAllLines(@"Input\Day1.txt"), int.Parse);
+            var map = new HashSet<int>();
+            int x = 0, y = 0, z = 0;
+
+            for(int i = 0; i < _expenses.Length; i++)
+            {
+                var temp1 = TOTAL - _expenses[i];
+
+                for(int j = 0; j < _expenses.Length; j++)
+                {
+                    var temp2 = temp1 - _expenses[j];
+
+                    if(map.Contains(temp2))
+                    {
+                        x = _expenses[i];
+                        y = _expenses[j];
+                        z = temp2;
+                    }
+                    else
+                    {
+                        map.Add(_expenses[j]);
+                    }
+                }
+                map.Clear();
+            }
+
+            return x * y * z;
+        }
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2020.Code.Day1;
+using static System.Console;
+
+WriteLine("---* Advent of Code 2020 *---");
+WriteLine("-~* Day 1 Part 1 *~-");
+WriteLine($"Multiply the 2 entries equal to 2020 together gives us {Part1.Solve()}");
+WriteLine($"Multiply the 3 entries equal to 2020 together gives us {Part2.Solve()}");
