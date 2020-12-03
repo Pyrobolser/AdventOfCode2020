8fb6f33d97712c157b547139ca94d46156e76b04
Francois Blondel <francois.blondel@outlook.com>
2020-12-02 18:41:28 -0600

    Day2 done!

---
+++ AdventOfCode2020/Code/Day2/Day2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.Code.Day2
+{
+    public static class Part1
+    {
+        public static int Solve()
+        {
+            var valids = 0;
+
+            foreach(var line in File.ReadAllLines(@"Input\Day2.txt"))
+            {
+                var chunks = line.Split(' ');
+                var limits = Array.ConvertAll(chunks[0].Split('-'), int.Parse);
+
+                if(chunks[2].GroupBy(x => x)
+                    .Any(g => g.Key == chunks[1][0]
+                    && g.Count() >= limits[0]
+                    && g.Count() <= limits[1]))
+                {
+                    valids++;
+                }
+            }
+
+            return valids;
+        }
+    }
+
+    public static class Part2
+    {
+
+        public static int Solve()
+        {
+            var valids = 0;
+
+            foreach (var line in File.ReadAllLines(@"Input\Day2.txt"))
+            {
+                var chunks = line.Split(' ');
+                var limits = Array.ConvertAll(chunks[0].Split('-'), int.Parse);
+
+                if ((chunks[2][limits[0] - 1] == chunks[1][0]) != (chunks[2][limits[1] - 1] == chunks[1][0]))
+                    valids++;
+            }
+
+            return valids;
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,7 +1,9 @@
-﻿using AdventOfCode2020.Code.Day1;
-using static System.Console;
+﻿using static System.Console;
 
 WriteLine("---* Advent of Code 2020 *---");
-WriteLine("-~* Day 1 Part 1 *~-");
-WriteLine($"Multiply the 2 entries equal to 2020 together gives us {Part1.Solve()}");
-WriteLine($"Multiply the 3 entries equal to 2020 together gives us {Part2.Solve()}");
+WriteLine("-~*-*-*^~*~ Day 1 ~*~^*-*-*~-");
+WriteLine($"Multiply the 2 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part1.Solve()}");
+WriteLine($"Multiply the 3 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part2.Solve()}");
+WriteLine("-~*-*-*^~*~ Day 2 ~*~^*-*-*~-");
+WriteLine($"There are {AdventOfCode2020.Code.Day2.Part1.Solve()} valid passwords");
+WriteLine($"There are {AdventOfCode2020.Code.Day2.Part2.Solve()} valid passwords with the new interpretation");
