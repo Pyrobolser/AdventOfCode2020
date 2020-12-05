df3823bf49e8113f3a4c91304551bdad87145c70
Pyrobolser <francois.blondel@outlook.com>
2020-12-05 11:24:56 -0600

    Day5!

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -2,8 +2,8 @@
 
 WriteLine("---* Advent of Code 2020 *---");
 WriteLine("-~*-*-*^~*~ Day 1 ~*~^*-*-*~-");
-WriteLine($"Multiply the 2 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part1.Solve()}");
-WriteLine($"Multiply the 3 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part2.Solve()}");
+WriteLine($"Multiplying the 2 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part1.Solve()}");
+WriteLine($"Multiplying the 3 entries equal to 2020 together gives us {AdventOfCode2020.Code.Day1.Part2.Solve()}");
 WriteLine("-~*-*-*^~*~ Day 2 ~*~^*-*-*~-");
 WriteLine($"There are {AdventOfCode2020.Code.Day2.Part1.Solve()} valid passwords");
 WriteLine($"There are {AdventOfCode2020.Code.Day2.Part2.Solve()} valid passwords with the new interpretation");
@@ -13,3 +13,8 @@ WriteLine($"Multiplying together all the trees from the listed slopes we get {Ad
 WriteLine("-~*-*-*^~*~ Day 4 ~*~^*-*-*~-");
 WriteLine($"There are {AdventOfCode2020.Code.Day4.Part1.Solve()} valid passports in the batch file");
 WriteLine($"There are {AdventOfCode2020.Code.Day4.Part2.Solve()} valid passports in the batch file with valid values");
+WriteLine("-~*-*-*^~*~ Day 5 ~*~^*-*-*~-");
+WriteLine($"The highest seat ID on a boarding pass is {AdventOfCode2020.Code.Day5.Part1.Solve()}");
+WriteLine($"The ID of the seat is {AdventOfCode2020.Code.Day5.Part2.Solve()}");
+
+
